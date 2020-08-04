0fdcfd77290ac45351b7ac73a93733026fa971c3
Armando Carrillo <ingcompuarmando@gmail.com>
2020-08-04 18:48:47 -0500

    Created table and detail view controllers

---
+++ Restaurant/MenuItemDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuItemDetailViewController.swift
+//  Restaurant
+//
+//  Created by Armando Carrillo on 04/08/20.
+//  Copyright © 2020 Armando Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
