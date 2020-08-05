a3e5cd0eb8d52c6546bc5e43b1cc3abe8f8682d0
Armando Carrillo <ingcompuarmando@gmail.com>
2020-08-05 17:31:21 -0500

    Parse the responses

--- Restaurant/Controllers/MenuController.swift
+++ Restaurant/Controllers/MenuController.swift
@@ -15,6 +15,14 @@ class MenuController {
         let categoryURL = baseURL.appendingPathComponent("categories")
         let task = URLSession.shared.dataTask(with: categoryURL) {
             (data, response, error) in
+            if let data = data,
+                let jsonDictionary = try?
+            JSONSerialization.jsonObject(with: data) as? [String: Any],
+                let categories = jsonDictionary["categories"] as? [String]{
+                completion(categories)
+            } else {
+                completion(nil)
+            }
             
         }
         task.resume()
@@ -27,6 +35,14 @@ class MenuController {
         let menuURL = components.url!
         let task = URLSession.shared.dataTask(with: menuURL){
             (data, response, error) in
+            
+            let jsonDecoder = JSONDecoder()
+            if let data = data,
+                let menuItems = try? jsonDecoder.decode(MenuItems.self, from: data) {
+                completion(menuItems.items)
+            } else {
+                completion(nil)
+            }
             
         }
         task.resume()
@@ -45,6 +61,13 @@ class MenuController {
         request.httpBody = jsonData
         let task = URLSession.shared.dataTask(with: request){ (data, response, error) in
             
+            let jsonDecoder = JSONDecoder()
+            if let data = data,
+                let preparationTime = try? jsonDecoder.decode(PreparationTime.self, from: data) {
+                completion(preparationTime.prepTime)
+            } else {
+                completion (nil)
+            }
         }
         task.resume()
     }
