ac9026f1976b5edf49f02d8b7e793c1258106d6e
Armando Carrillo <ingcompuarmando@gmail.com>
2020-08-12 13:49:59 -0500

    Set the URL cache size

--- Restaurant/AppDelegate.swift
+++ Restaurant/AppDelegate.swift
@@ -19,17 +19,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             orderTabBarItem?.badgeValue = nil
         case let count:
             orderTabBarItem?.badgeValue = String(count)
-        
-        
     }
     }
+    
     
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
        
-       
+        let temporaryDirectory = NSTemporaryDirectory()
+        let urlCache = URLCache(memoryCapacity: 25_000_000, diskCapacity: 50_000_000, diskPath: temporaryDirectory)
+        URLCache.shared = urlCache
         
             NotificationCenter.default.addObserver(self, selector: #selector(updateOrderBadge), name: MenuController.orderUpdatedNotification, object: nil)
+        
+        //orderTabBarItem = (self.window!.rootViewController! as! UITabBarController).viewControllers![1].tabBarItem
         
         
         
