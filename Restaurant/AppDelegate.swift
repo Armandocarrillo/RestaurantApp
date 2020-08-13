1ca79f89204ab895b5324dc4cbad564902d86bb3
Armando Carrillo <ingcompuarmando@gmail.com>
2020-08-12 19:36:37 -0500

    Opt into state restoration in the app delegate

--- Restaurant/AppDelegate.swift
+++ Restaurant/AppDelegate.swift
@@ -61,6 +61,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
        MenuController.shared.saveOrder()
     }
+    
+    func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
 
 
 }
